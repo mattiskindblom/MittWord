3980ec1758d64618f5957cbffe159e6984f80430
Mattis Kindblom <mattis.kindblom@elev.ga.ntig.se>
2021-02-12 10:54:21 +0100

    Initial commit

---
+++ src/sample/GUIManagerExample.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIManagerExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUIManagerExample");
+        frame.setContentPane(new GUIManagerExample().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton clearButton;
+
+    public GUIManagerExample() {
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
