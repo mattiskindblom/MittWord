611a0d8a4bbe72469cf8269b5a0804f05a8d5ac3
Mattis Kindblom <mattis.kindblom@elev.ga.ntig.se>
2021-02-19 11:17:08 +0100

    Lektion ?2 Försökte lägga till en meny men det krånglade så jag tog bort det och nästa lektion kommer jag bara lägga på att försöka göra en fungerande meny

--- src/sample/GUIManagerExample.java
+++ src/sample/GUIManagerExample.java
@@ -1,28 +1,32 @@
 package sample;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+
 
 public class GUIManagerExample {
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("GUIManagerExample");
-        frame.setContentPane(new GUIManagerExample().panel1);
+        GUIManagerExample gui = new GUIManagerExample();
+        frame.setContentPane(gui.panel1);
+        frame.setJMenuBar(gui.menuBar);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
+
+    private JButton clearButton;
+    private JButton CopyButton;
     private JTextArea textArea1;
     private JPanel panel1;
-    private JButton newButton;
-    private JButton clearButton;
 
-    public GUIManagerExample() {
-        clearButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-    }
+    private static JMenuBar menuBar;
+    private JMenu menu, submenu;
+    private JMenuItem menuItem, menuItemNew;
+    private JRadioButtonMenuItem rbMenuItem;
+    private JCheckBoxMenuItem cbMenuItem;
 }
+
+
+
+
