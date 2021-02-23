6c797248e00c3772e369ad0ab76b9e45ad09a99d
Mattis Kindblom <mattis.kindblom@elev.ga.ntig.se>
2021-02-23 11:17:49 +0100

    Lektion ?3 Försökte göra så om man trycker på en knapp så ska den skriva en text i text rutan.

--- src/sample/GUIManagerExample.java
+++ src/sample/GUIManagerExample.java
@@ -5,20 +5,35 @@ import javax.swing.*;
 
 public class GUIManagerExample {
 
+    public GUIManagerExample() {
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("GUIManagerExample");
         GUIManagerExample gui = new GUIManagerExample();
         frame.setContentPane(gui.panel1);
-        frame.setJMenuBar(gui.menuBar);
+        frame.setJMenuBar(menuBar);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
     private JButton clearButton;
-    private JButton CopyButton;
+    private JButton AddTextButton;
     private JTextArea textArea1;
     private JPanel panel1;
+
+
+    JTextArea textArea = new JTextArea(
+            "You pressed the unforgivable button. " +
+                    "You are now \"DOOMED\" to hell, " +
+                    "if you're still reading it's because " +
+                    "this text is so random it got you curious."
+    );
+    textArea.setLineWrap(true);
+textArea.setWrapStyleWord(true);
+
+
 
     private static JMenuBar menuBar;
     private JMenu menu, submenu;
